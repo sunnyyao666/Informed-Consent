387e36758639155adec3004bdc22772629fc2e93
姚鸿韬 <18302010017@fudan.edu.cn>
2021-03-25 22:43:08 +0800

    按新文档更新接口实现，新增redis用于记录(手机号，验证码）键值对

--- server/src/main/java/cn/edu/fudan/biological/controller/user/UserAccountController.java
+++ server/src/main/java/cn/edu/fudan/biological/controller/user/UserAccountController.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import redis.clients.jedis.Jedis;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @program: biological
@@ -20,18 +24,37 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class UserAccountController {
     private final UserInfoRepository userInfoRepository;
     private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final Jedis jedis = new Jedis("localhost");
 
     @Autowired
     public UserAccountController(UserInfoRepository userInfoRepository) {
         this.userInfoRepository = userInfoRepository;
     }
 
-    //    微信openid ?
+    //    验证码发送
+    @GetMapping("/code")
+    public MyResponse userRegisterGetCode(@RequestBody UserAccountRequest userAccountRequest) {
+        String username = userAccountRequest.getUsername();
+        User_info userInfo = userInfoRepository.findByUsername(username);
+        if (userInfo != null) return MyResponse.fail("用户名重复", 1101);
+
+        String code = "123456";
+        jedis.set(username, code);
+        jedis.expire(username, 300);
+
+        return MyResponse.success();
+    }
+
+    //    验证码验证
     @PostMapping("/register")
     public MyResponse userRegister(@RequestBody UserAccountRequest userAccountRequest) {
         String username = userAccountRequest.getUsername();
         User_info userInfo = userInfoRepository.findByUsername(username);
         if (userInfo != null) return MyResponse.fail("用户名重复", 1101);
+
+        String code = userAccountRequest.getCode();
+        String redisCode = jedis.get(username);
+        if (redisCode == null || !redisCode.equals(code)) return MyResponse.fail("验证码错误", -1);
 
         String password = userAccountRequest.getPassword();
         String email = userAccountRequest.getEmail();
@@ -52,7 +75,6 @@ public class UserAccountController {
         return MyResponse.success();
     }
 
-    //    验证码
     @PostMapping("/signature")
     public MyResponse setSignature(@RequestBody UserAccountRequest userAccountRequest) {
         String username = userAccountRequest.getUsername();
@@ -62,18 +84,61 @@ public class UserAccountController {
         String password = userAccountRequest.getPassword();
         if (!passwordEncoder.matches(password, userInfo.getPassword())) return MyResponse.fail("密码错误", 1103);
 
-        String signature = userAccountRequest.getSignature();
-        if (signature == null) return MyResponse.success();
+        int[] gesture = userAccountRequest.getGesture();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int value : gesture) stringBuilder.append(value);
+        String signature = stringBuilder.toString();
 
         userInfo.setSignature(signature);
         userInfoRepository.save(userInfo);
         return MyResponse.success();
     }
 
+    @GetMapping("/hasSignature")
+    public MyResponse hasSignature(@RequestBody UserAccountRequest userAccountRequest) {
+        String username = userAccountRequest.getUsername();
+        User_info userInfo = userInfoRepository.findByUsername(username);
+        if (userInfo == null) return MyResponse.fail("用户名不存在", 1102);
+
+        String password = userAccountRequest.getPassword();
+        if (!passwordEncoder.matches(password, userInfo.getPassword())) return MyResponse.fail("密码错误", 1103);
+
+        String signature = userInfo.getSignature();
+        Map<String, Boolean> data = new HashMap<>();
+        data.put("hasSignature", !(signature == null || "".equals(signature)));
+        return MyResponse.success("成功", data);
+    }
+
+    @PostMapping("/uncheckedSignature")
+    public MyResponse checkSignature(@RequestBody UserAccountRequest userAccountRequest) {
+        String username = userAccountRequest.getUsername();
+        User_info userInfo = userInfoRepository.findByUsername(username);
+        if (userInfo == null) return MyResponse.fail("用户名不存在", 1102);
+
+        String password = userAccountRequest.getPassword();
+        if (!passwordEncoder.matches(password, userInfo.getPassword())) return MyResponse.fail("密码错误", 1103);
+
+        int[] gesture = userAccountRequest.getGesture();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int value : gesture) stringBuilder.append(value);
+        String signature = stringBuilder.toString();
+
+        if (signature.equals(userInfo.getSignature())) return MyResponse.success();
+        else return MyResponse.fail("手势密码错误", 1103);
+    }
+
     //    验证码发送
     @GetMapping("/signatureCode")
-    public MyResponse sendSignatureCode() {
+    public MyResponse signatureGetCode(@RequestBody UserAccountRequest userAccountRequest) {
+        String username = userAccountRequest.getUsername();
+        User_info userInfo = userInfoRepository.findByUsername(username);
+        if (userInfo == null) return MyResponse.fail("用户名不存在", 1102);
+
+        String password = userAccountRequest.getPassword();
+        if (!passwordEncoder.matches(password, userInfo.getPassword())) return MyResponse.fail("密码错误", 1103);
+
         return MyResponse.success();
     }
+
 }
 
