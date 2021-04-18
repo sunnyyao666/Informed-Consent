57f2ca4f160c174deaa626573647a8ac46e83351
Shen Zhengyu <18302010009@fudan.edu.cn>
2021-04-18 10:04:06 +0800

    增加定时任务,协助修改任务状态

--- server/src/main/java/cn/edu/fudan/biological/BiologicalApplication.java
+++ server/src/main/java/cn/edu/fudan/biological/BiologicalApplication.java
@@ -24,8 +24,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -37,6 +39,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  **/
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableJpaAuditing
+@EnableCaching // 启用缓存功能
+@EnableScheduling // 开启定时任务功能
 @Slf4j
 public class BiologicalApplication {
 
---
+++ server/src/main/java/cn/edu/fudan/biological/task/AutoChangeProjectStatusTask.java
@@ -0,0 +1,46 @@
+package cn.edu.fudan.biological.task;
+
+import cn.edu.fudan.biological.domain.Project_info;
+import cn.edu.fudan.biological.repository.ProjectInfoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @program: biological
+ * @description: 根据时间决定结束任务
+ * @author: Shen Zhengyu
+ * @create: 2021-04-18 09:45
+ **/
+@Component
+@Slf4j
+public class AutoChangeProjectStatusTask {
+    private final ProjectInfoRepository projectInfoRepository;
+    @Autowired
+    public AutoChangeProjectStatusTask(ProjectInfoRepository projectInfoRepository) {
+        this.projectInfoRepository = projectInfoRepository;
+    }
+
+    @Autowired
+
+    @Scheduled(cron = "0 0 0 */1 * ?")
+    public void execute() throws InterruptedException {
+        log.info("开始执行周期任务，将过期项目改变状态");
+        log.info("线程休眠3秒");
+        Thread.sleep(3000);
+        for (Project_info project_info : projectInfoRepository.findAll()) {
+            if ("ongoing".equals(project_info.getStatus())){
+                Date finishDate = project_info.getEndTime();
+                Date nowDate = new Date();
+                if (nowDate.after(finishDate)){
+                    project_info.setStatus("finished");
+                    projectInfoRepository.save(project_info);
+                }
+            }
+        }
+    }
+}
